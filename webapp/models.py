15998f3c3cdd0ba317c2caf3dbed35dcea02c3be
Dylan Moreland <dylan.c.moreland@gmail.com>
2021-05-12 16:53:47 -0700

    Add timestamps to Tag model

--- webapp/models.py
+++ webapp/models.py
@@ -2,13 +2,6 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
-class Tag(models.Model):
-    tag_id = models.ForeignKey(DataPoint.tag_id)
-    serial_num = models.CharField(max_length=150)
-
-    def __str__(self):
-        return "Tag %d is attached to serial number %s" % (self.tag_id, self.serial_num)
-
 class DataPoint(models.Model):
     tag_id = models.IntegerField()
     timestamp = models.DateTimeField()
@@ -20,9 +13,20 @@ class DataPoint(models.Model):
     def __str__(self):
         return "Tag %d at %s" % (self.tag_id, self.timestamp)
 
+class Tag(models.Model):
+    tag_id = models.IntegerField()
+    serial_num = models.CharField(max_length=150)
+    valid_after = models.DateTimeField()
+    valid_before = models.DateTimeField(null=True)
+
+
+    def __str__(self):
+        return "Tag %d is attached to serial number %s" % (self.tag_id, self.serial_num)
+
 class ClinicItem(models.Model):
-    tag = models.ForeignKey(Tag.tag_id)
-    timestamp_added_to_clinic = models.DateTimeField()
-    timestamp_removed_from_clinic = models.DateTimeField(null=True)
+    tag_id = models.IntegerField()
+    serial_num = models.CharField(max_length=150)
+    added_to_clinic = models.DateTimeField()
+    removed_from_clinic = models.DateTimeField(null=True)
     from_zone = ArrayField(models.CharField(max_length=150))
     problem = models.CharField(max_length=1000)
