337a6f500b5e649d780ac4535b32bc908992a21f
Dylan Moreland <dylan.c.moreland@gmail.com>
2021-05-18 00:39:29 -0700

    Add data endpoint to URL conf

--- webapp/urls.py
+++ webapp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('manager', views.manager, name='manager'),
     path('executive', views.executive, name='executive'),
     path('ie', views.ie, name='ie'),
+    path('data', views.data, name='data') # used for returning metrics and other data via AJAX requests on the browser side
 ]
