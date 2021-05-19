1a319f7313b0ad84287a53e55bbaa6119e49f33f
Dylan Moreland <dylan.c.moreland@gmail.com>
2021-05-19 16:16:22 -0700

    Add ClinicItem to admin page

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import DataPoint
-from .models import Tag
+from .models import DataPoint, Tag, ClinicItem
 
 # Register your models here.
 admin.site.register(DataPoint)
